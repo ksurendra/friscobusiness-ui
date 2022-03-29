c79651c50ea62cee8f1bd86ebbadea6a79e63e1a
Suren Konathala <suren@Surens-MacBook-Pro.local>
2022-03-29 16:48:29 -0500

    created business detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BusinessDetailComponent } from './components/business/business-detail/business-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'business-detail', component: BusinessDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
