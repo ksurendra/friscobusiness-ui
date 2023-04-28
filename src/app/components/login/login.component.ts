9923c3eaa88d532a5e2123b21669fea4c7270000
Suren K <konathalasuren@gmail.com>
2023-04-27 21:10:54 -0500

    inital working version for auth0

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    public auth: AuthService    
+  ) {}
+
+  ngOnInit() {}
+
+  loginWithRedirect() {
+    this.auth.loginWithRedirect();
+  }
+  
 }
